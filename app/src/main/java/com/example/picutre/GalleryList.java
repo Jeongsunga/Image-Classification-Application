daaf0d4ea01e19f208bcffcdb1c5460f693d4305
ristukaJJang <ghwjd5585@gmail.com>
2024-05-22 12:19:07 +0900

    리사이클러뷰 화면(사진, 폴더 이름, 사진 갯수) 중에서 폴더 이름 가져오기까지 성공

--- app/src/main/java/com/example/picutre/GalleryList.java
+++ app/src/main/java/com/example/picutre/GalleryList.java
@@ -1,13 +1,15 @@
 package com.example.picutre;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,40 +21,47 @@ import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import android.widget.Toast;
+import java.util.Map;
 
 public class GalleryList extends AppCompatActivity {
 
-
+/*
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+    private static final int REQUEST_CODE_READ_EXTERNAL_STORAGE = 1;
+
+
     File file2 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
     String[] folders2 = file2.list();
-
     File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
     String[] folders = file.list();
+    ListView listview;
+    */
 
-    private ListView listview;
-
-
+    private static final int REQUEST_PERMISSION = 100;
+    private RecyclerView recyclerView;
+    private listAdaptor listAdaptor;
+    private List<String> folderNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_gallery_list);
-
-
+/*
         listview = findViewById(R.id.listview);
 
         List<String> data1 = new ArrayList<>();
-
-
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data1);
-
         listview.setAdapter(adapter1);
+
 
         for(int i =0; i< folders.length; i++) {
             if(folders[i].startsWith(".") || folders[i].isEmpty()) continue;
@@ -78,44 +87,15 @@ public class GalleryList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+*/
 
-/*
-        ArrayList<Data> list = new ArrayList<>();
-        for(int i = 0; i<folders.length; i++) {
-            list.add(new Data(folders[i], i));
-        }
-        for(int i=0; i<folders2.length; i++) {
-            list.add(new Data(folders2[i], i));
-        }
+        recyclerView = findViewById(R.id.recylcerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        folderNames = new ArrayList<>();
+        listAdaptor = new listAdaptor(folderNames);
+        recyclerView.setAdapter(listAdaptor);
 
-        class listAdapter extends BaseAdapter {
-            List<Data> lists;
-
-            public listAdapter(List<Data> lists) {
-                this.lists = lists;
-            }
-
-            @Override
-            public int getCount() {
-                return lists.size();
-            }
-            public Object getItem(int i) {
-                return lists.get(i);
-            }
-
-            @Override
-            public long getItemId(int i) {
-                return i;
-            }
-
-            @Override
-            public View getView(int i, ViewGroup viewGroup) {
-                return null;
-            }
-        }
-        */
-
-
+        loadGalleryFolders();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -124,6 +104,24 @@ public class GalleryList extends AppCompatActivity {
         });
     }
 
+    private void loadGalleryFolders() {
+        HashSet<String> folderSet = new HashSet<>();
+        String[] projection = {MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
 
+        if (cursor != null) {
+            int folderNameIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+            while (cursor.moveToNext()) {
+                String folderName = cursor.getString(folderNameIndex);
+                folderSet.add(folderName);
+            }
+            cursor.close();
+        }
+
+        folderNames.clear();
+        folderNames.addAll(folderSet);
+        listAdaptor.notifyDataSetChanged();
+    }
 
 }
